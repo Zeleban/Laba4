a4a0b8259f397b4f4d016ba13ffab173eb05f324
Zel <zelenskyi.vladislav@gmail.com>
2019-08-14 21:38:25 +0300

    Laba4 начало

---
+++ src/ua/zp/brainacad/LearnDebug.java
@@ -0,0 +1,11 @@
+package ua.zp.brainacad;
+
+public class LearnDebug {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.printrln("end");
+            
+        }
+    }
+}
